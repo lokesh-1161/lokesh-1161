242cb1036f3695791ed3b433b5a3f880101ae57e
Lokesh K <lokesh.k1161@gmail.com>
2025-05-20 12:46:23 +0530

    Initial commit

---
+++ Training_Application/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Training_Application.Models;
+
+namespace Training_Application.Controllers
+{
+    public class ProductController : Controller
+    {
+        private static List<Dictionary<string, string>> products = new List<Dictionary<string, string>>
+        {
+            new Dictionary<string, string> { { "Id", "101" }, { "Name", "Laptop" }, { "Price", "1200" } },
+            new Dictionary<string, string> { { "Id", "102" }, { "Name", "Mouse" }, { "Price", "25" } }
+        };
+
+        public ActionResult Index()
+        {
+            ViewBag.ProductList = products;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(FormCollection form)
+        {
+            var newProduct = new Dictionary<string, string>
+            {
+                { "Id", form["Id"] },
+                { "Name", form["Name"] },
+                { "Price", form["Price"] }
+            };
+
+            products.Add(newProduct);
+            return RedirectToAction("Index");
+        }
+    }
+    }
---
+++ Training_Application/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Training_Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
